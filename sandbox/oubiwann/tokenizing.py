520a1e762f68bce65eb71aea64fadbd75591d1ed
Duncan McGreggor <duncan@dreamhost.com>
2012-06-02 13:16:51 -0400

    Added some initial tokenizing code.

---
+++ sandbox/oubiwann/tokenizing.py
@@ -0,0 +1,14 @@
+from nltk import tag, tokenize
+import nltk.data
+
+
+d = """"Hello, Martha. It isn't cocktail-time yet, is it?" The girl at the
+table spoke without raising her head, almost without moving her lips, as
+though she were afraid that the slightest breath would disturb the flaky
+stuff in front of her."""
+
+t = tokenize.word_tokenize(d)
+pos_tags_1 = tag.pos_tag(t)
+
+tagger = nltk.data.load(tag._POS_TAGGER)
+pos_tags_2 = tagger.tag(t)
